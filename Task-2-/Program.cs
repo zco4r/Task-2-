29c3cf4500a84d149d3c34f5c951430f62e5d935
ammaralazki <alazkiyasser3@gmail.com>
2026-07-02 15:35:12 +0400

    task1

---
+++ Task-2-/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_2_;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter a starting number: ");
+
+
+        if (int.TryParse(Console.ReadLine(), out int startNumber))
+        {
+            for (int i = startNumber; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
